c8d63d8a0970dac376bf0b95cf00896a8328317c
Tuhin Chakma <122518680+iamtuhinchakma@users.noreply.github.com>
2023-08-27 19:57:22 +0600

    Update RunnerLoginForm.java

--- Chapter Eight/src/problem_26/RunnerLoginForm.java
+++ Chapter Eight/src/problem_26/RunnerLoginForm.java
@@ -3,13 +3,11 @@ package problem_26;
 import java.util.Scanner;
 
 public class RunnerLoginForm {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         LoginForm form = new LoginForm("mayuresh_95", "LoveCoding_95");
         Scanner in = new Scanner(System.in);
 
-        while(!form.isLoggedIn())
-        {
+        while(!form.isLoggedIn()){
             System.out.println("Login Form");
 
             System.out.print("Enter Username: ");
